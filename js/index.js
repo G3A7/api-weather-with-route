496241feb7b379b0a9e12b7829a8d37e05322740
Ahmed <mnaz004587@gmail.com>
2024-12-11 00:46:57 +0200

    api-test

--- js/index.js
+++ js/index.js
@@ -1,144 +1,58 @@
-const login = document.getElementById("login");
-const email = document.getElementById("email");
-const pass = document.getElementById("pass");
-const eye = document.getElementById("eye");
-const eyeSlash = document.getElementById("eye-slash");
-// console.log(eye, eyeSlash);
-const inputs = document.querySelectorAll(".col-md-4 input");
-// console.log(inputs)
-const regex = {
-  email: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
-  pass: /^.{6,10}$/,
-};
-// console.log(localStorage.getItem("userLogin"))
-let arrLogin = JSON.parse(localStorage.getItem("userLogin"));
-if (!localStorage.getItem("userLogin")) {
-  arrLogin = [];
-}
-eye.addEventListener("click", (e) => {
-  pass.setAttribute("type", "text");
-  eyeSlash.classList.replace("d-none", "d-block");
-  eye.classList.replace("d-block", "d-none");
-});
-eyeSlash.addEventListener("click", (e) => {
-  pass.setAttribute("type", "password");
+//api Key  363f0ee9576c4499974210006241012
+// console.log(new Date("2024-12-11 00:30"));
+// let c = new Date("2024-12-11 00:30");
 
-  eyeSlash.classList.replace("d-block", "d-none");
-  eye.classList.replace("d-none", "d-block");
-});
-inputs.forEach((e) => {
-  e.addEventListener("input", (e) => {
-    validationsInput(e.target);
-  });
-});
+// console.log(c.toString().split(' ')[1]);
+// console.log(c.toString().split(' ')[2]);
+// console.log(c.toString().split(' ')[0]);
 
-login.addEventListener("click", () => {
-  if (CheckValidation()) {
-    let objData = {
-      // name: name.value,
-      email: email.value,
-      pass: pass.value,
-    };
-    const validUser = arrLogin.find((e) => {
-      return e.email == objData.email;
-    });
-    // const Index = arrLogin.findIndex((e) => {
-    //   return e.email == objData.email;
-    // });
-    console.log(validUser);
-    if (validUser) {
-      if (validUser.pass == objData.pass) {
-        location.assign(`./home.html?idx=${validUser.id}`);
-      } else {
-        pass.classList.add("is-invalid");
-        Swal.fire({
-          html: `<div class="box">
-              <div class="box-header">
-                  <div class="circle">
-                      <span></span>
-                      <span></span>
-                      <span></span>
+
+
+
+(async function getAPi() {
+  const res = await fetch(
+    "http://api.weatherapi.com/v1/forecast.json?key=363f0ee9576c4499974210006241012&q=cairo&days=2"
+  );
+  const data = await res.json();
+  // document
+  //   .querySelector("#img")
+  //   .setAttribute("src", `https:${data.forecast.forecastday[0].day.condition.icon}`);
+  console.log(data); // درحه الحراره الحاليه
+  console.log(data.forecast.forecastday[0].day.maxtemp_c);
+  console.log(data.forecast.forecastday[0].day.mintemp_c);
+  console.log(data.forecast.forecastday[0].day.condition.icon);
+  console.log(data.forecast.forecastday[0].day.condition.text);
+})();
+
+/*
+ <div class="col-md-4">
+            <div class="inner-weather">
+              <div class="weather-title d-flex justify-content-between p-2">
+                <span>Tuesday</span>
+                <span>10December</span>
+              </div>
+              <div class="weather-content">
+                <h3>Cairo</h3>
+                <h4 class="h1">22.1C</h4>
+                <div class="status">
+                  <img id="img" src="" class="w-100 d-block" alt="" />
+                </div>
+                <span>Sunny</span>
+                <div class="icons d-flex align-items-center gap-2">
+                  <div class="icon">
+                    <i class="fa-solid fa-umbrella"></i>
+                    <span>20%</span>
                   </div>
-                  <div class="box-body text-start   pt-4">
-                      <ul class=' w-75 mt-5 mx-auto text-center'>
-                          <li class='text-nowrap'>
-                             <strong>Pass:</strong> <i class='text-danger'>Wrong</i> 
-                          </li>
-                          
-                      </ul>
+                  <div class="icon">
+                    <i class="fa-solid fa-wind"></i> 
+                    <span>20%</span>
                   </div>
+                  <div class="icon">
+                    <i class="fa-solid fa-compass"></i>
+                    <span>20%</span>
+                  </div>
+                </div>
               </div>
-            </div>`,
-        });
-      }
-    } else {
-      Swal.fire({
-        html: `<div class="box">
-          <div class="box-header">
-              <div class="circle">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-              </div>
-              <div class="box-body text-start   pt-4">
-                  <ul class=' w-75 mt-5 mx-auto text-center'>
-                      <li class='text-nowrap'>
-                         <strong>Email:</strong> <i class='text-danger'>Is Already </i> 
-                      </li>
-                      
-                  </ul>
-              </div>
+            </div>
           </div>
-        </div>`,
-      });
-    }
-  } else {
-    Swal.fire({
-      html: `<div class="box">
-    <div class="box-header">
-        <div class="circle">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-        <div class="box-body text-start   pt-4">
-            <ul class=' w-75 mt-5 mx-auto'>
-              
-                 <li  class='text-nowrap'>
-                   <strong>Email:</strong> <i class='text-danger'>Exampl@gmail.com</i> 
-                </li>
-                <li  class='text-nowrap'>
-                   <strong>Password:</strong> <i class='text-danger'>must be more than 6</i> 
-                </li>
-            </ul>
-        </div>
-    </div>
-  </div>`,
-    });
-  }
-});
-
-function validationsInput(el) {
-  if (regex[el.id].test(el.value)) {
-    el.classList.add("is-valid");
-    el.classList.remove("is-invalid");
-    return true;
-  } else {
-    el.classList.remove("is-valid");
-    el.classList.add("is-invalid");
-    return false;
-  }
-}
-function CheckValidation() {
-  let flag = true;
-
-  if (!validationsInput(email)) {
-    flag = false;
-  }
-  if (!validationsInput(pass)) {
-    flag = false;
-  }
-  //    validationsInput(name) && validationsInput(email) && validationsInput(pass);
-  return flag;
-}
-``;
+*/
